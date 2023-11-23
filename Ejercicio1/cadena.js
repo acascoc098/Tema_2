6c1838a87fb3212fbbb9527c52d6e620c7ccc339
acascoc098 <acascoc098@g.educaand.es>
2023-11-23 14:41:38 +0100

    Hasta el apartado q.

--- Ejercicio1/cadena.js
+++ Ejercicio1/cadena.js
@@ -41,32 +41,59 @@ console.log(texto2.toUpperCase());
 console.log("-----------------------------------------------------------------------------");
 
 // i. Recorre con un bucle el texto caracter a caracter imprimiéndolo.
+for(let i=0; i < texto2.length; i++) {
+    console.log(texto2[i]);
+}
 console.log("-----------------------------------------------------------------------------");
 
 // j. Busca una subcadena en un texto.
+let subcadena = texto2.substring(2,6);
+const esta = texto2.includes(subcadena);
+console.log(`¿Está la subcadena ${subcadena} en el texto? ${esta}`)
 console.log("-----------------------------------------------------------------------------");
 
 // k. Extrae en una variable la subcadena desde la posición 3 hasta el final del
 // texto.
+let subtexto = texto2.substring(3,texto2.length);
+console.log(subtexto);
 console.log("-----------------------------------------------------------------------------");
 
 // l. Extrae en una variable la subcadena desde la posición 3 hasta la primera
 // ocurrencia de una palabra en el texto.
+let ocur = texto2.indexOf("bits");
+let subtexto2 = texto2.substring(3,ocur);
+console.log(subtexto2);
 console.log("-----------------------------------------------------------------------------");
 
 // m. Reemplaza todos los espacios del texto por un -
+console.log(texto2.replaceAll(" ","-"));
 console.log("-----------------------------------------------------------------------------");
 
 // n. Elimina los espacios antes y después del texto.
+let textoespacios = '                      Tipo string. Secuencia de caracteres de 16 bits en Unicode.                                     ';
+console.log(textoespacios);
+console.log(textoespacios.trim());
 console.log("-----------------------------------------------------------------------------");
 
 // o. Crea una cadena que no tenga ningún espacio partiendo de otra dada.
+let cadenasin = texto2.replaceAll(" ",".");
+console.log(cadenasin);
 console.log("-----------------------------------------------------------------------------");
 
 // p. Crea una función que invierta una cadena de texto.
+let cadena = "alrevés";
+let cadenareves = [...cadena].reverse().join('');
+console.log(cadenareves);
 console.log("-----------------------------------------------------------------------------");
 
 // q. Usa una expresión regular para comprobar que la cadena tiene números.
+let regex = /\w/;
+let cadenanum = "Cadena12"
+const tienennum = regex.test(cadenanum);
+let cadenanum2 = "CadenaSin"
+const tienennum2 = regex.test(cadenanum2);
+console.log(tienennum);
+console.log(tienennum2);
 console.log("-----------------------------------------------------------------------------");
 
 // r. Usa una expresión regular para comprobar que la cadena termina en punto.
