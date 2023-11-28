5b81d178ab0869bdbb96f866bb0cd156b8749c56
acascoc098 <acascoc098@g.educaand.es>
2023-11-28 18:54:51 +0100

    Creamos ejercicio 4.

---
+++ Ejercicio 4/Ejercicio4.js
@@ -0,0 +1,9 @@
+// 4. Crea un programa que genere un array con 1000 números aleatorios del 0 al 99.
+// a. Crea una función que calcule la media aritmética.
+// b. Calcula la frecuencia de cada número del 0 al 99. Es decir, si el número 0
+// aparece 80 veces en el array de 1000 posiciones, se guardará un 80 en la
+// posición 0 del nuevo array. Si el número 1 aparece 50 veces se guardará un
+// 50 en la posición 1, etc.
+// c. Crea una función que ordene el array de menor a mayor sin usar métodos de
+// Javascript.
+// d. Ahora usa una función javascript para realizar la ordenación
