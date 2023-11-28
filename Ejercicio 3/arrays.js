022df6716c109fdc66d0d69fbb89663847c855ce
acascoc098 <acascoc098@g.educaand.es>
2023-11-28 17:36:34 +0100

    Creamos el documento para el ejercicio 3.

---
+++ Ejercicio 3/arrays.js
@@ -0,0 +1,44 @@
+// a. Crea un array "datos" vacío con un literal.
+// b. Añade a "datos" los números del 1 al 50 con un bucle for.
+// c. Elimina los elementos del 25 al 50 asignando un nuevo tamaño a la
+// propiedad length
+// d. Usa el operador spread para hacer una copia del array anterior.
+// e. Crea un array de tamaño 50 con el constructor Array.
+// f. Copia el array anterior a otro con la factoría from.
+// g. Crea un array multidimensional de 10 filas (i) y 10 columnas (j). Inicializa
+// cada celda con el valor i*j.
+// h. Crea un array con la factoría of con los números del 1 al 5. Después añade
+// un elemento en la posición 10 y otro en la 50. Recorre el array con for
+// imprimiendo valores y después con forEach. ¿Cuál es la diferencia? ¿Cuál es
+// el tamaño del array?.
+// i. Elimina dos elementos con delete.
+// j. Calcula el producto de todos los números del array "datos" con forEach.
+// k. Cada elemento x del array datos debe cambiarse por x*x. Usa forEach
+// l. Crea un nuevo array con map recorriendo cada elemento x de "datos", donde
+// cada elemento sea un string "El valor es: x". Usa template string.
+// m. Crea un array nuevo mediante map que incremente cada elemento de
+// "datos" en 5 unidades.
+// n. Mediante filter quédate con los números impares en un nuevo array impares.
+// o. Usa find para buscar el número 13.
+// p. Usa every para comprobar si todos los números son positivos.
+// q. Calcula la sumatoria del array datos mediante reduce.
+// r. Calcula el valor más pequeño del array mediante reduce.
+// s. Usa flat para aplanar el array multidimensional que creaste anteriormente.
+// t. Tenemos la cadena: "Vamos a usar flatMap. Es igual que map. Pero aplana
+// los arrays". Separa mediante split las distintas frases. Después mediante
+// map quita los espacios sobrantes (trim). A continuación usa flatMap para
+// extraer todas las palabras de cada frase en un único array.
+// u. Crea el array a [1,2,3,4,5] y b [6,7,8,9,10] con literales. Concatena los arrays
+// a y b con concat. Después con operador spread. Crea una variable const
+// cola. Usa unshift y shift para añadir y quitar elementos. Dado el array
+// resultante de la concatenación de a y b, obten el subarray desde el índice 2
+// al penúltimo elemento (slice). Usa splice para quitar los 2 últimos elementos
+// de un array.
+// v. Rellena con fill un array de 100 elementos con -1
+// w. Crea un array de cadenas. Busca con indexOf una cadena.
+// x. Comprueba si la cadena "hola" está dentro del array anterior.
+// y. Ordena la lista de cadenas anterior de forma alfabética con sort.
+// z. Crea un array vacío de 50 posiciones. Con forEach asigna valores aleatorios
+// entre 0 y 100. Después ordena con sort de menor a mayor. Cambia y ordena
+// de mayor a menor.
+// aa. Usa reverse para invertir el array anterior.
