deb94886ddb154ca8513a9c304a542a0c6f15a93
acascoc098 <acascoc098@g.educaand.es>
2023-11-28 16:38:54 +0100

    Creamos el js del ejercicio 2.

---
+++ Ejercicio 2/desestructura.js
@@ -0,0 +1,24 @@
+//a. Desestructura día, mes y año e imprime la fecha dado el array ['06', 'Octubre','2021'];
+console.log("-----------------------------------------------------------------------------");
+//b. Dado un array de números, desestructura los números en posicionesimpares.
+console.log("-----------------------------------------------------------------------------");
+//c. Desestructura el primer número, el segundo y el resto en otra variable.
+console.log("-----------------------------------------------------------------------------");
+//d. Desestructura nombre, apellidos y teléfono del siguiente objeto.
+const person = { nombre: 'Luis', apellidos: 'Molina', telefono: '+34666554433'};
+console.log("-----------------------------------------------------------------------------");
+//e. Cambia el siguiente bucle para desestructurar cada entrada e imprimir llave,valor por separado.
+for(const propertie of Object.entries(person)) { console.log(propertie); }
+console.log("-----------------------------------------------------------------------------");
+//f. Dado [{x: 1, y: 2}, {x: 3, y: 4}], desestructura los puntos en las variables x1,y1, x2, y2
+console.log("-----------------------------------------------------------------------------");
+//g. Crea una función a la que le pasas un único objeto como parámetro con 5
+//propiedades cualquiera incluida la propiedad nombre y apellidos.
+//Desestructura en la función la propiedad nombre y apellidos e imprime el nombre completo.
+console.log("-----------------------------------------------------------------------------");
+//h. Dados dos objetos, combínalos en uno solo utilizando el operador spread. Después elimina alguna de las propiedades.
+console.log("-----------------------------------------------------------------------------");
+//i. Crea una función que retorna un array con 3 valores. Usa la función y desestructura los valores.
+console.log("-----------------------------------------------------------------------------");
+//j. Realiza una clonación profunda de un objeto que contiene otros objetos o arrays como propiedades.
+console.log("-----------------------------------------------------------------------------");
